f6c6d8f7029bc8e35dacd96b241ad1f80fcf9860
Nate <me@n8m.us>
2025-02-01 09:54:15 -0400

    init repo

---
+++ config/config.go
@@ -0,0 +1,73 @@
+package config
+
+import (
+	"bytes"
+	"fmt"
+	"os"
+
+	"gopkg.in/yaml.v3"
+)
+
+type (
+	// Syncer contains the configuration for the p2p syncer.
+	Syncer struct {
+		Address    string   `yaml:"address,omitempty"`
+		Bootstrap  bool     `yaml:"bootstrap,omitempty"`
+		EnableUPnP bool     `yaml:"enableUPnP,omitempty"`
+		Peers      []string `yaml:"peers,omitempty"`
+	}
+
+	// Consensus contains the configuration for the consensus set.
+	Consensus struct {
+		Network        string `yaml:"network,omitempty"`
+		IndexBatchSize int    `yaml:"indexBatchSize,omitempty"`
+	}
+
+	// FileLog configures the file output of the logger.
+	FileLog struct {
+		Enabled bool   `yaml:"enabled,omitempty"`
+		Level   string `yaml:"level,omitempty"` // override the file log level
+		Format  string `yaml:"format,omitempty"`
+		// Path is the path of the log file.
+		Path string `yaml:"path,omitempty"`
+	}
+
+	// StdOutLog configures the standard output of the logger.
+	StdOutLog struct {
+		Level      string `yaml:"level,omitempty"` // override the stdout log level
+		Enabled    bool   `yaml:"enabled,omitempty"`
+		Format     string `yaml:"format,omitempty"`
+		EnableANSI bool   `yaml:"enableANSI,omitempty"` //nolint:tagliatelle
+	}
+
+	// Log contains the configuration for the logger.
+	Log struct {
+		StdOut StdOutLog `yaml:"stdout,omitempty"`
+		File   FileLog   `yaml:"file,omitempty"`
+	}
+
+	// Config contains the configuration for the indexer
+	Config struct {
+		Directory      string `yaml:"directory,omitempty"`
+		RecoveryPhrase string `yaml:"recoveryPhrase,omitempty"`
+
+		Syncer    Syncer    `yaml:"syncer"`
+		Consensus Consensus `yaml:"consensus"`
+		Log       Log       `yaml:"log"`
+	}
+)
+
+// LoadFile loads the configuration from the provided file path.
+// If the file does not exist or cannot be decoded, an error is returned.
+func LoadFile(fp string, cfg *Config) error {
+	buf, err := os.ReadFile(fp)
+	if err != nil {
+		return fmt.Errorf("failed to read config file: %w", err)
+	}
+
+	r := bytes.NewReader(buf)
+	dec := yaml.NewDecoder(r)
+	dec.KnownFields(true)
+
+	return dec.Decode(cfg)
+}
---
+++ go.mod
@@ -0,0 +1,19 @@
+module go.sia.tech/indexd
+
+go 1.23.4
+
+require (
+	go.sia.tech/core v0.9.1
+	go.sia.tech/coreutils v0.10.1
+	go.uber.org/zap v1.27.0
+	gopkg.in/yaml.v3 v3.0.1
+	lukechampine.com/flagg v1.1.1
+	lukechampine.com/upnp v0.3.0
+)
+
+require (
+	go.uber.org/multierr v1.11.0 // indirect
+	golang.org/x/crypto v0.32.0 // indirect
+	golang.org/x/sys v0.29.0 // indirect
+	lukechampine.com/frand v1.5.1 // indirect
+)
