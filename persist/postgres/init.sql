3a6548b6e23718101db027345d940c9decf154b5
Chris Schinnerl <chris@sia.tech>
2025-04-14 15:16:43 +0200

    update schema

--- persist/postgres/init.sql
+++ persist/postgres/init.sql
@@ -216,4 +216,5 @@ CREATE INDEX sectors_host_id_idx ON sectors(host_id);
 CREATE INDEX sectors_contract_id_idx ON sectors(contract_id);
 
 -- speed up integrity check query
+CREATE INDEX sectors_next_integrity_check_idx ON sectors(next_integrity_check ASC);
 CREATE INDEX sectors_host_id_next_integrity_check_idx ON sectors(host_id, next_integrity_check ASC);
