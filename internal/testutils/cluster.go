468b5e7b75c60ead69c9d68f1fd003a34493a9db
PJ <peterjan.brone@gmail.com>
2025-11-04 10:14:45 +0100

    e2e: fix tests

--- internal/testutils/cluster.go
+++ internal/testutils/cluster.go
@@ -9,6 +9,7 @@ import (
 	"go.sia.tech/core/consensus"
 	"go.sia.tech/core/types"
 	"go.sia.tech/coreutils/testutil"
+	"go.sia.tech/indexd/contracts"
 	"go.uber.org/zap"
 )
 
@@ -179,4 +180,36 @@ func (c *Cluster) NewHosts(t testing.TB, n int) []*Host {
 		hosts = append(hosts, cn.NewHost(t, pk, c.log.Named("host-"+pk.PublicKey().String())))
 	}
 	return hosts
+}
+
+// WaitForContracts waits until a contract is formed with every host in the cluster
+func (c *Cluster) WaitForContracts(t *testing.T) {
+	t.Helper()
+	cm := c.Indexer.Contracts()
+
+	required := make(map[types.PublicKey]struct{})
+	for _, h := range c.Hosts {
+		required[h.PublicKey()] = struct{}{}
+	}
+
+	for range 100 {
+		contracts, err := cm.Contracts(t.Context(), 0, math.MaxInt, contracts.WithGood(true), contracts.WithRevisable(true))
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		seen := make(map[types.PublicKey]struct{})
+		for _, c := range contracts {
+			if _, ok := required[c.HostKey]; ok {
+				seen[c.HostKey] = struct{}{}
+			}
+		}
+
+		if len(seen) == len(required) {
+			return
+		}
+		time.Sleep(100 * time.Millisecond)
+	}
+
+	t.Fatalf("not all contracts formed after timeout")
 }
